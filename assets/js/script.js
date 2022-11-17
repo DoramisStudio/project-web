5e92d33bceef42e2a5c1e8ad33daf213860e2d03
Jonathan T <jonathantriastono@gmail.com>
2022-11-17 09:05:56 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
 const image_list = [
     "../assets/img/Gojo-Satoru.jpg",
-    "../assets/img/periodic-table.jpg"
+    "../assets/img/periodic-table.jpg",
+    "../assets/img/kunjungan-industri-2022.jpg"
 ];
 
 var image_slide_container = document.getElementById('image_slide');
 
 var image_slide_pos = image_list.length; 
+var img = image_slide_container.children[0];
 
-function switchWall() {
-    for (let i = 0; i >= (image_slide_pos - 1); i++) {
-        console.log(i);
-    }
+let intervalSlide;
+
+function nextSlide() {
+    if (image_slide_pos < (image_list.length - 1)) {image_slide_pos++;}
+    else {image_slide_pos = 0;}
+    
+    updateWall();
+    wait(1000);
 }
+			
+function prevSlide() {
+    if (image_slide_pos < 1) {image_slide_pos = (image_list.length - 1);}
+    else {image_slide_pos--;}
+    
+    updateWall();
+    wait(1000);
+}
+
+function updateWall() {
+    window.clearInterval(intervalSlide);
+    intervalSlide = setInterval(nextSlide, 8000);
+    img.classList.remove('slide-transition');
+    setTimeout(function(){img.classList.add('slide-transition');
+    img.setAttribute('src', image_list[image_slide_pos]);}, 100)
+}
+
+intervalSlide = setInterval(nextSlide, 8000);
+nextSlide();
